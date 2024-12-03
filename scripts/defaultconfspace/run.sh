6d5e921f72f75c97c02daf8cd55f34aff8a1ca97
Raphael <raphael.zipperer@gmail.com>
2024-12-03 22:52:39 +0100

    spelling mistake

--- scripts/defaultconfspace/run.sh
+++ scripts/defaultconfspace/run.sh
@@ -6,8 +6,8 @@ conda activate SMAC
 
 
 for i in $(seq 0 34); do
-    OUT="/nfs/home/rzipperer/git/Kissat_hyperparamoptimization/scriptout/defaultconf/hellman/$i.out"
-    ERR="/nfs/home/rzipperer/git/Kissat_hyperparamoptimization/scriptout/defaultconf/hellman/$i.err"
+    OUT="/nfs/home/rzipperer/git/Kissat_hyperparamoptimization/scriptout/defaultconf/naur/$i.out"
+    ERR="/nfs/home/rzipperer/git/Kissat_hyperparamoptimization/scriptout/defaultconf/naur/$i.err"
 
     if [ ! -f $OUT ]; then
         echo "python3 /nfs/home/rzipperer/git/Kissat_hyperparamoptimization/scripts/defaultconfspace/defaultconfig.py $i 5000 20 10 > $OUT 2> $ERR" 
