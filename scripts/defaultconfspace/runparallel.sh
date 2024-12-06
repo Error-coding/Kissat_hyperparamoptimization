ad5ce6b25a8c84266aef1b2fcdf9c6256586143e
Raphael <raphael.zipperer@gmail.com>
2024-12-06 16:58:43 +0100

    Default config Parallel script

---
+++ scripts/defaultconfspace/runparallel.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+spack env activate smac3
+source /nfs/home/rzipperer/.bashrc
+conda activate SMAC
+cd /nfs/home/rzipperer/git/Kissat_hyperparamoptimization/scripts/parallel
+
+OUT="/nfs/home/rzipperer/git/Kissat_hyperparamoptimization/scriptout/defaultconf/blum/parallel/$1.out"
+ERR="/nfs/home/rzipperer/git/Kissat_hyperparamoptimization/scriptout/defaultconf/blum/parallel/$1.err"
+
+python3 /nfs/home/rzipperer/git/Kissat_hyperparamoptimization/scripts/defaultconfs/defaultparallel.py $1 $2 $3 $4 $5 > $OUT 2> $ERR
