4bece65b1a2fc89a665a6b9023b5ab533f713fd2
Raphael <raphael.zipperer@gmail.com>
2024-12-18 18:57:49 +0100

    Typo

--- scripts/toplevel/8_35.sh
+++ scripts/toplevel/8_35.sh
@@ -3,5 +3,6 @@ spack env activate smac3
 source /nfs/home/rzipperer/.bashrc
 conda activate SMAC
 
-comm=$"sh /nfs/home/rzipperer/git/Kissat_hyperparamoptimization/scripts/toplevel/8_21.sh\nsh /nfs/home/rzipperer/git/Kissat_hyperparamoptimization/scripts/toplevel/22_35.sh"
+comm=$"sh /nfs/home/rzipperer/git/Kissat_hyperparamoptimization/scripts/toplevel/8_21.sh
+sh /nfs/home/rzipperer/git/Kissat_hyperparamoptimization/scripts/toplevel/22_35.sh"
 echo "$comm" | parallel -j 2
