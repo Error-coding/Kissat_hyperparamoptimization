45064b517b4f3eff9ac7a7b7f07143bcb4ab158a
Raphael <raphael.zipperer@gmail.com>
2025-01-07 03:08:12 +0100

    Typo

--- scripts/extendedconfspace/run17.sh
+++ scripts/extendedconfspace/run17.sh
@@ -4,6 +4,6 @@ spack env activate smac3
 source /nfs/home/rzipperer/.bashrc
 conda activate SMAC
 
-OUT="/nfs/home/rzipperer/git/Kissat_hyperparamoptimization/scriptout/extend/liskov/$i.out"
-ERR="/nfs/home/rzipperer/git/Kissat_hyperparamoptimization/scriptout/extend/liskov/$i.err"
-python3 /nfs/home/rzipperer/git/Kissat_hyperparamoptimization/scripts/extendedconfspace/extended_optimization.py $i 1800 200 32 > $OUT 2> $ERR
+OUT="/nfs/home/rzipperer/git/Kissat_hyperparamoptimization/scriptout/extend/liskov/17.out"
+ERR="/nfs/home/rzipperer/git/Kissat_hyperparamoptimization/scriptout/extend/liskov/17.err"
+python3 /nfs/home/rzipperer/git/Kissat_hyperparamoptimization/scripts/extendedconfspace/extended_optimization.py 17 1800 200 32 > $OUT 2> $ERR
