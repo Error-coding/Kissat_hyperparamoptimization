85f702870de05bcd06db4b1000857c738afaf608
Raphael <raphael.zipperer@gmail.com>
2025-02-16 13:43:02 +0300

    Top 29, greedy elimination

--- scripts/anniclassifier/run.sh
+++ scripts/anniclassifier/run.sh
@@ -3,7 +3,7 @@ spack env activate smac3
 source /nfs/home/rzipperer/.bashrc
 conda activate SMAC
 
-OUT="/nfs/home/rzipperer/git/Kissat_hyperparamoptimization/scriptout/anniclassifier/liskov/random_forest_top40.out"
-ERR="/nfs/home/rzipperer/git/Kissat_hyperparamoptimization/scriptout/anniclassifier/liskov/random_forest_top40.err"
+OUT="/nfs/home/rzipperer/git/Kissat_hyperparamoptimization/scriptout/anniclassifier/liskov/random_forest_top29.out"
+ERR="/nfs/home/rzipperer/git/Kissat_hyperparamoptimization/scriptout/anniclassifier/liskov/random_forest_top29.err"
 
 python3 /nfs/home/rzipperer/git/Kissat_hyperparamoptimization/scripts/anniclassifier/anni.py  > $OUT 2> $ERR
