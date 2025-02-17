00eb306d8824b3065c7412cac7acc033ac80384b
Raphael <raphael.zipperer@gmail.com>
2025-02-17 23:08:25 +0300

    Minimal hitting set, daaase labels

--- scripts/anniclassifier/run.sh
+++ scripts/anniclassifier/run.sh
@@ -3,7 +3,7 @@ spack env activate smac3
 source /nfs/home/rzipperer/.bashrc
 conda activate SMAC
 
-OUT="/nfs/home/rzipperer/git/Kissat_hyperparamoptimization/scriptout/anniclassifier/liskov/random_forest_top31.out"
-ERR="/nfs/home/rzipperer/git/Kissat_hyperparamoptimization/scriptout/anniclassifier/liskov/random_forest_top31.err"
+OUT="/nfs/home/rzipperer/git/Kissat_hyperparamoptimization/scriptout/anniclassifier/liskov/random_forest_minhitset.out"
+ERR="/nfs/home/rzipperer/git/Kissat_hyperparamoptimization/scriptout/anniclassifier/liskov/random_forest_minhitset.err"
 
 python3 /nfs/home/rzipperer/git/Kissat_hyperparamoptimization/scripts/anniclassifier/anni.py  > $OUT 2> $ERR
