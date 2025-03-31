5c82d37bde534ef78a25a3b6134506f3d7b407ad
Raphael <raphael.zipperer@gmail.com>
2025-03-31 23:36:46 +0200

    {'backbone': 2, 'bump': 1, 'chrono': 1, 'congruence': 0, 'eliminate': 0, 'extract': 0, 'factor': 1, 'fastel': 1, 'forward': 0, 'lucky': 0, 'phase': 1, 'phasesaving': 0, 'preprocess': 1, 'probe': 0, 'randec': 0, 'reluctant': 1, 'reorder': 0, 'rephase': 1, 'restart': 1, 'stable': 1, 'substitute': 1, 'sweep': 1, 'target': 2, 'transitive': 1, 'vivify': 1, 'warmup': 1}

--- scripts/defaultconfanni/run.sh
+++ scripts/defaultconfanni/run.sh
@@ -3,7 +3,7 @@ spack env activate smac3
 source /nfs/home/rzipperer/.bashrc
 conda activate SMAC
 
-OUT="/nfs/home/rzipperer/git/Kissat_hyperparamoptimization/scriptout/defaultconfanni/liskov/anni4.out"
-ERR="/nfs/home/rzipperer/git/Kissat_hyperparamoptimization/scriptout/defaultconfanni/liskov/anni4.err"
+OUT="/nfs/home/rzipperer/git/Kissat_hyperparamoptimization/scriptout/defaultconfanni/liskov/anni5.out"
+ERR="/nfs/home/rzipperer/git/Kissat_hyperparamoptimization/scriptout/defaultconfanni/liskov/anni5.err"
 
 python3 /nfs/home/rzipperer/git/Kissat_hyperparamoptimization/scripts/defaultconfanni/parallelanni.py  > $OUT 2> $ERR
