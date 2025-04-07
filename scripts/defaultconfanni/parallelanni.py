31c8dd08fbfd2b618da352bc8f77d535c86c02ec
Raphael <raphael.zipperer@gmail.com>
2025-04-07 19:09:52 +0200

    {'backbone': 0, 'bump': 1, 'chrono': 1, 'congruence': 1, 'eliminate': 1, 'extract': 0, 'factor': 1, 'fastel': 0, 'forward': 1, 'lucky': 0, 'phase': 1, 'phasesaving': 1, 'preprocess': 0, 'probe': 1, 'randec': 1, 'reluctant': 1, 'reorder': 0, 'rephase': 0, 'restart': 1, 'stable': 0, 'substitute': 0, 'sweep': 1, 'target': 0, 'transitive': 0, 'vivify': 1, 'warmup': 1}

--- scripts/defaultconfanni/parallelanni.py
+++ scripts/defaultconfanni/parallelanni.py
@@ -39,7 +39,7 @@ def parse_dict_string(dict_string):
     return parsed_dict
 
 # Example usage
-dict_string = "{'backbone': 2, 'bump': 1, 'chrono': 1, 'congruence': 0, 'eliminate': 0, 'extract': 0, 'factor': 0, 'fastel': 1, 'forward': 0, 'lucky': 0, 'phase': 1, 'phasesaving': 1, 'preprocess': 1, 'probe': 1, 'randec': 0, 'reluctant': 0, 'reorder': 0, 'rephase': 0, 'restart': 1, 'stable': 0, 'substitute': 0, 'sweep': 0, 'target': 0, 'transitive': 1, 'vivify': 1, 'warmup': 0}"
+dict_string = "{'backbone': 0, 'bump': 1, 'chrono': 1, 'congruence': 1, 'eliminate': 1, 'extract': 0, 'factor': 1, 'fastel': 0, 'forward': 1, 'lucky': 0, 'phase': 1, 'phasesaving': 1, 'preprocess': 0, 'probe': 1, 'randec': 1, 'reluctant': 1, 'reorder': 0, 'rephase': 0, 'restart': 1, 'stable': 0, 'substitute': 0, 'sweep': 1, 'target': 0, 'transitive': 0, 'vivify': 1, 'warmup': 1}"
 
 
 parsed_dict = parse_dict_string(dict_string)
--- scripts/defaultconfanni/run.sh
+++ scripts/defaultconfanni/run.sh
@@ -3,7 +3,7 @@ spack env activate smac3
 source /nfs/home/rzipperer/.bashrc
 conda activate SMAC
 
-OUT="/nfs/home/rzipperer/git/Kissat_hyperparamoptimization/scriptout/defaultconfanni/liskov/anni8.out"
-ERR="/nfs/home/rzipperer/git/Kissat_hyperparamoptimization/scriptout/defaultconfanni/liskov/anni8.err"
+OUT="/nfs/home/rzipperer/git/Kissat_hyperparamoptimization/scriptout/defaultconfanni/liskov/anni9.out"
+ERR="/nfs/home/rzipperer/git/Kissat_hyperparamoptimization/scriptout/defaultconfanni/liskov/anni9.err"
 
 python3 /nfs/home/rzipperer/git/Kissat_hyperparamoptimization/scripts/defaultconfanni/parallelanni.py  > $OUT 2> $ERR
